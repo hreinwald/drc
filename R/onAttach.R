4c9d6aca2fe70b4d1d7acb7999e1a9841f191162
Hannes Reinwald <115988583+hreinwald@users.noreply.github.com>
2026-04-13 16:44:38 +0200

    Update R/onAttach.R to include the package attach hook with additional information

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,30 +2,28 @@
 #' @keywords internal
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("\n===============================================")
-    packageStartupMessage("  'drc' has been loaded")
-    packageStartupMessage("  Analysis of Dose-Response Data")
-    packageStartupMessage("  Version 3.3.0")
-    packageStartupMessage("===============================================\n")
-
-    packageStartupMessage("Developers:")
-    packageStartupMessage("  - Christian Ritz (ritz@bioassay.dk)")
-    packageStartupMessage("  - Jens C. Streibig (streibig@bioassay.dk)")
-    packageStartupMessage("  - Hannes Reinwald (hannes.reinwald@bayer.com)\n")
-
-    packageStartupMessage("Please cite 'drc' if used for a publication:\n")
-    packageStartupMessage("  Ritz, C., Jensen, S. M., Gerhard, D., Streibig, J. C. (2019)")
-    packageStartupMessage("  Dose-Response Analysis Using R. CRC Press\n")
-    packageStartupMessage("Additional references:")
-    packageStartupMessage("  - Ritz, C., et al. (2015). Dose-Response Analysis Using R.")
-    packageStartupMessage("    PLOS ONE, 10(12), e0146021.")
-    packageStartupMessage("  - Ritz, C. and Streibig, J. C. (2005). Bioassay Analysis using R.")
-    packageStartupMessage("    Journal of Statistical Software, 12(5), 1-22.\n")
-
-    packageStartupMessage("For citation formats, type: citation('drc')")
-    packageStartupMessage("For R citation, type: citation()\n")
-
-    packageStartupMessage("Bug reports and issues:")
-    packageStartupMessage("  https://github.com/hreinwald/drc/issues/\n")
-    packageStartupMessage("===============================================\n")
+    packageStartupMessage(paste0(
+        "\n===============================================\n",
+        "  'drc' has been loaded\n",
+        "  Analysis of Dose-Response Data\n",
+        "  Version 3.3.1\n",
+        "===============================================\n\n",
+        "Developers:\n",
+        "  - Christian Ritz (ritz@bioassay.dk)\n",
+        "  - Jens C. Streibig (streibig@bioassay.dk)\n",
+        "  - Hannes Reinwald (hannes.reinwald@bayer.com)\n\n",
+        "Please cite 'drc' if used for a publication:\n\n",
+        "  Ritz, C., Jensen, S. M., Gerhard, D., Streibig, J. C. (2019)\n",
+        "  Dose-Response Analysis Using R. CRC Press\n\n",
+        "Additional references:\n",
+        "  - Ritz, C., et al. (2015). Dose-Response Analysis Using R.\n",
+        "    PLOS ONE, 10(12), e0146021.\n",
+        "  - Ritz, C. and Streibig, J. C. (2005). Bioassay Analysis using R.\n",
+        "    Journal of Statistical Software, 12(5), 1-22.\n\n",
+        "For citation formats, type: citation('drc')\n",
+        "For R citation, type: citation()\n\n",
+        "Bug reports and issues:\n",
+        "  https://github.com/hreinwald/drc/issues/\n",
+        "===============================================\n"
+    ))
 }
